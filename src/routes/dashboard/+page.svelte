081055a5b2dd5cfe9d2072cc6d982e35106531c2
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-01-11 07:20:54 +0800

    Fix formatting

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -17,13 +17,42 @@
         >
           {link.label}
         </a>
-        <DescriptiveLink links={[
-          { href: "/dashboard/human_resources/employee", label: "Employees", icon: "groups", description: "Show the list of current employees." },
-          { href: "/dashboard/human_resources/job", label: "Jobs", icon: "work", description: "Explore and manage job-related information." },
-          { href: "/dashboard/human_resources/shift", label: "Shifts", icon: "schedule", description: "View and manage employee work schedules." },
-          { href: "/dashboard/human_resources/timesheet", label: "Timesheets", icon: "punch_clock", description: "Track and manage employee working hours and attendance." },
-          { href: "/dashboard/human_resources/health-exam", label: "Health Exams", icon: "medical_information", description: "Track and manage employee health examination records." },
-        ]} />
+        <DescriptiveLink
+          links={[
+            {
+              href: "/dashboard/human_resources/employee",
+              label: "Employees",
+              icon: "groups",
+              description: "Show the list of current employees.",
+            },
+            {
+              href: "/dashboard/human_resources/job",
+              label: "Jobs",
+              icon: "work",
+              description: "Explore and manage job-related information.",
+            },
+            {
+              href: "/dashboard/human_resources/shift",
+              label: "Shifts",
+              icon: "schedule",
+              description: "View and manage employee work schedules.",
+            },
+            {
+              href: "/dashboard/human_resources/timesheet",
+              label: "Timesheets",
+              icon: "punch_clock",
+              description:
+                "Track and manage employee working hours and attendance.",
+            },
+            {
+              href: "/dashboard/human_resources/health-exam",
+              label: "Health Exams",
+              icon: "medical_information",
+              description:
+                "Track and manage employee health examination records.",
+            },
+          ]}
+        />
       </div>
     {:else if link.href === "/dashboard/teams"}
       <div class="w-full">
@@ -33,11 +62,28 @@
         >
           Project Management
         </a>
-        <DescriptiveLink links={[
-          { href: "/dashboard/teams", label: "Teams", icon: "groups", description: "Show the list of current teams." },
-          { href: "/dashboard/projects", label: "Projects", icon: "work", description: "Explore and manage project-related information." },
-          { href: "/dashboard/software", label: "Software", icon: "code", description: "View and manage software-related information." },
-        ]} />
+        <DescriptiveLink
+          links={[
+            {
+              href: "/dashboard/teams",
+              label: "Teams",
+              icon: "groups",
+              description: "Show the list of current teams.",
+            },
+            {
+              href: "/dashboard/projects",
+              label: "Projects",
+              icon: "work",
+              description: "Explore and manage project-related information.",
+            },
+            {
+              href: "/dashboard/software",
+              label: "Software",
+              icon: "code",
+              description: "View and manage software-related information.",
+            },
+          ]}
+        />
       </div>
     {/if}
   {/each}
